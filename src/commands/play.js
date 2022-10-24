b93563fed1262ff72db34d4773ce6e8d6318e6bf
Luke <lukematusiak@protonmail.com>
2022-10-23 21:36:19 -0700

    add button selection

--- src/commands/play.js
+++ src/commands/play.js
@@ -1,33 +1,66 @@
-const { SlashCommandBuilder } = require('discord.js');
+const { SlashCommandBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle, ComponentType } = require('discord.js');
 const { guildList } = require('../helpers/state');
 const yt = require('../helpers/youtube');
 
 const YT_TOKEN = process.env['YT_TOKEN'];
 
-async function execute(interaction) {
-  const q = interaction.options.getString('query');
-  await interaction.reply({ content: `Searcing youtube for ${q}`});
-  const channelId = interaction.member.voice.channel.id;
-  const channelName = interaction.member.voice.channel.name;
-  const guildId = interaction.member.guild.id;
-  let searchData = null;
 
+async function getSelection(interaction) {
+  const q = interaction.options.getString('query');
+  let searchData = null;
   try {
     searchData = await yt.search(q, 5, YT_TOKEN);
     if(searchData === null) {
       await interaction.editReply('Failed to query youtube');
-      throw err('Failed to load youtube audio');
+      throw err('Failed to query youtube');
     }
+  } catch(err) {
+    throw err;
+  }
+
+  const buttons = [];
+  searchData.forEach(result => {
+    buttons.push(new ButtonBuilder()
+      .setCustomId(`${result.id}`)
+      .setLabel(`${result.name}`)
+      .setStyle(ButtonStyle.Primary)
+    )
+  })
+
+  const buttonRow = new ActionRowBuilder().addComponents(...buttons);
+  const message = await interaction.editReply({content: 'Pick a song', components: [buttonRow], fetchReply: true});
+  let choice = null;
+  try {
+    choice = await message.awaitMessageComponent({ time: 30_000, componentType: ComponentType.Button });
+  } catch(err) {
+    interaction.editReply({ content: 'Timeout waiting for input', components: [] })
+    return null;
+  }
+  return [choice.customId, choice.component.label]
+}
+
+async function execute(interaction) {
+  const q = interaction.options.getString('query');
+  await interaction.reply({ content: `Searcing youtube for ${q}` });
+  const channelId = interaction.member.voice.channel.id;
+  const channelName = interaction.member.voice.channel.name;
+  const guildId = interaction.member.guild.id;
+
+  let [songId, songName] = await getSelection(interaction);
+
+  if(!songId) return null;
+  try {
     if(!guildList.activeGuilds[`${guildId}`]) {
       console.log('Have to init guild');
       await guildList.initGuild(guildId, channelId, interaction);
     }
-    guildList.addSong(guildId, searchData[0].name, searchData[0].id);
+    guildList.addSong(guildId, songName, songId);
     console.log(guildList.activeGuilds[`${guildId}`].queue);
   } catch(err) {
     throw err;
   }
-  await interaction.editReply(`Added ${searchData[0].name} to queue`);
+  
+  interaction.editReply({content: `Added ${songName} to queue`, components: []})
 }
 
 const command = new SlashCommandBuilder()
