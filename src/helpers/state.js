52f02223f810d907729838d905d9e86b70abca20
ItsICG <adads1632@gmail.com>
2022-10-29 21:04:38 -0400

    icg

--- src/helpers/state.js
+++ src/helpers/state.js
@@ -26,8 +26,7 @@ const GuildQueues = function() {
           songName: '',
           youtubeId: '',
         }
-      ],
-      channelId: ''
+      ]
     }
   }
 
---
+++ src/services/voice-channel-monitor.js
@@ -0,0 +1,25 @@
+
+
+function voiceChannelMonitor(guild){
+
+    this.on = true;
+    this.vChatCleaner = new voiceChatCleaner(guildId);
+
+
+    this.start = ()=>{
+        this.loop();
+    }
+    this.loop = ()=>{
+
+    }
+}
+
+function voiceChatCleaner(guildId){
+
+
+
+
+}
+function voiceChatMessageClearer(voiceChannelId,guildId) {
+    
+}
