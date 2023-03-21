ddffce08171bcb1befd56f4ceca6fb8c00798945
Luke Matusiak <lukematusiak@pm.me>
2023-03-21 16:59:23 -0700

    push separate dev and prod builds

--- container/build.sh
+++ container/build.sh
@@ -4,11 +4,18 @@ TAG="$(date +%Y-%m-%d_%H.%M.%S)"
 REG="docker-reg.service.consul:5000"
 PUSH="${PUSH:-0}"
 
-DOCKER_BUILDKIT=1 docker build -t "$APP":latest -t "$APP:$TAG" -f ./Dockerfile ..
+DOCKER_BUILDKIT=1 docker build -t "$APP:$TAG" -t "$APP:latest" -f ./Dockerfile ..
 
 if [ "$PUSH" = "1" ]; then
   docker tag "$APP:$TAG" "$REG/$APP:$TAG"
-  docker tag "$APP:$TAG" "$REG/$APP:latest"
   docker push "$REG/$APP:$TAG"
-  docker push "$REG/$APP:latest"
+  if [ "$DEBUG" == "1" ]; then 
+    docker tag "$APP:$TAG" "$REG/$APP:dev"
+    docker push "$REG/$APP:dev"
+  else
+    docker tag "$APP:$TAG" "$REG/$APP:latest"
+    docker tag "$APP:$TAG" "$REG/$APP:prod"
+    docker push "$REG/$APP:latest"
+    docker push "$REG/$APP:prod"
+  fi
 fi
