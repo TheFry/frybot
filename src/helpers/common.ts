959c2f4f394cca919c4abdfdcd8b838b2b655aa2
Luke Matusiak <lukematusiak@pm.me>
2023-03-25 13:26:04 -0700

    add common module

---
+++ src/helpers/common.ts
@@ -0,0 +1,25 @@
+import { Client } from 'discord.js';
+
+export class DiscordClient extends Client { commands: any };
+
+function usage(missing: string) {
+  console.log(`
+  Missing variable ${missing}
+	Usage: DC_TOKEN=<bot token> \\
+		  DC_CLIENT=<bot client id> \\
+		  G_ID=<bot guild id> \\
+		  YT_TOKEN=<youtube key> \\
+		  MONGO_CONN_STRING=<mongodb conn string> \\
+		  node ${__dirname}/main.js
+	`);
+}
+
+export function checkVars() {
+  process.env['DC_TOKEN'] || usage('DC_TOKEN'); 
+  process.env['DC_CLIENT'] || usage('DC_CLIENT');
+  process.env['YT_TOKEN'] || usage('YT_TOKEN');
+  process.env['G_ID'] || usage('G_ID');
+  // process.env['MONGO_CONN_STRING'] || usage();
+}
+
+
