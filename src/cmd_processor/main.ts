5655ebd1e3be2e8998dfd46f00a9740a8439578a
Luke Matusiak <lukematusiak@pm.me>
2023-08-08 00:28:29 -0700

    hmore robust command error handling

--- src/cmd_processor/main.ts
+++ src/cmd_processor/main.ts
@@ -1,5 +1,5 @@
 import loadCommands from '../helpers/load-commands';
-import { Client, Collection, GatewayIntentBits, Interaction, InteractionEditReplyOptions } from 'discord.js';
+import { Client, Collection, GatewayIntentBits, Interaction } from 'discord.js';
 import { checkVars, DiscordClient } from '../helpers/common';
 import { addInteraction } from '../helpers/interactions';
 import * as redis from '../helpers/redis';
@@ -13,36 +13,44 @@ const client: DiscordClient = new Client({ intents: [GatewayIntentBits.Guilds, G
 
 
 redis.connect()
-	// .then(() => { redis.connect(); })
-	.then(() => { client.login(DC_TOKEN) })
-	.catch((err) => { 
-		console.log(err);
-		process.exit(1);
-	})
+  .then(() => { client.login(DC_TOKEN) })
+  .catch((err) => { 
+    console.log(err);
+    process.exit(1);
+  })
 
 
 client.once('ready', async () => {
-	console.log('Client logged in!');
-	client.commands = new Collection();
-	loadCommands(client, DC_TOKEN, DC_CLIENT, '../cmd_processor/commands', G_ID);
+  console.log('Client logged in!');
+  client.commands = new Collection();
+  loadCommands(client, DC_TOKEN, DC_CLIENT, '../cmd_processor/commands', G_ID);
 });
 
 
 client.on('interactionCreate', async (interaction: Interaction) => {
-	if (!interaction.isChatInputCommand()) return;
-	await addInteraction(interaction);
-	const interactionClient = interaction.client as DiscordClient;
-	const command: any = interactionClient.commands.get(interaction.commandName);
+  if (!interaction.isChatInputCommand()) return;
+  await addInteraction(interaction);
+  const interactionClient = interaction.client as DiscordClient;
+  const command: any = interactionClient.commands.get(interaction.commandName);
 
-	if (!command) {
-		interaction.reply('Command not registered!');
-		return;
-	}
+  if (!command) {
+    interaction.reply('Command not registered!');
+    return;
+  }
 
-	try {
-		await command.execute(interaction);
-	} catch (error) {
-		console.error(error);
-		await interaction.editReply({ content: 'There was an error while executing this command!', ephemeral: true } as InteractionEditReplyOptions);
-	}
+  try {
+    await command.execute(interaction);
+  } catch (error) {
+    console.error(error);
+    let errMsg = { content: 'There was an error while executing this command!', ephemeral: true };
+    try {
+      if(await interaction.replied) {
+        await interaction.followUp(errMsg); 
+      } else {
+        await interaction.reply(errMsg);
+      }
+    } catch(err) {
+      console.log(`Error trying to send error ??? - ${err}`)
+    }
+  }
 })
