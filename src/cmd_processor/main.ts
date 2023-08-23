f2e868afeab52ca53bc4868d66a7d8394641a8e1
Luke Matusiak <lukematusiak@pm.me>
2023-08-22 20:46:10 -0700

    add interaction queue processor

--- src/cmd_processor/main.ts
+++ src/cmd_processor/main.ts
@@ -1,8 +1,10 @@
 import loadCommands from '../helpers/load-commands';
 import { Client, Collection, GatewayIntentBits, Interaction } from 'discord.js';
-import { checkVars, DiscordClient } from '../helpers/common';
-import { addInteraction } from '../helpers/interactions';
+import { checkVars, DiscordClient, INTERACTION_QUEUE_KEY } from '../helpers/common';
+import { addInteraction, DiscordResponse, interactions } from '../helpers/interactions';
 import { newClient as newRedisClient } from '../helpers/redis';
+import { dequeue } from '../helpers/message_queue';
+import { rmSync } from 'fs';
 
 checkVars();
 const DC_TOKEN = process.env['DC_TOKEN'] || '';
@@ -24,6 +26,7 @@ client.once('ready', async () => {
   await newRedisClient();
   client.commands = new Collection();
   loadCommands(client, DC_TOKEN, DC_CLIENT, '../cmd_processor/commands', G_ID);
+  respond();
 });
 
 
@@ -44,13 +47,60 @@ client.on('interactionCreate', async (interaction: Interaction) => {
     console.error(error);
     let errMsg = { content: 'There was an error while executing this command!', ephemeral: true };
     try {
-      if(await interaction.replied) {
-        await interaction.followUp(errMsg); 
+      if(interaction.replied) {
+        await interaction.editReply(errMsg); 
       } else {
         await interaction.reply(errMsg);
       }
     } catch(err) {
-      console.log(`Error trying to send error ??? - ${err}`)
+      console.log(`Error trying to send error ??? - ${err}`);
     }
   }
 })
+
+
+async function respond() {
+  let watch = true;
+
+  while(watch) {
+    let res = (await dequeue(INTERACTION_QUEUE_KEY, 1, 0))[0];
+    if(res && res.error) {
+      console.log(`Error dequeueing from interaction queue - ${res.error}`);
+      continue;
+    }
+
+    if(!res.message) {
+      console.log(`Error dequeueing from interaction queue - no message object`);
+      continue;
+    }
+
+    let { content, files, interactionId } = res.message as DiscordResponse;
+    let interaction = interactions[interactionId];
+    if(interactions) {
+      if(interaction.isChatInputCommand() || interaction.isModalSubmit()) {
+        try {
+          if(interaction.replied) {
+            await interaction.editReply({ content: content ? content : '', files: files ? files : [] });
+          } else {
+            await interaction.editReply({ content: content ? content : '', files: files ? files : [] });
+          }
+        } catch(err) {
+          console.log(`Interaction Reply error - ${err}`)
+          continue;
+        }
+        
+        // For now just delete files after sending
+        if(files !== undefined) {
+          files.forEach(file => {
+            try {
+              rmSync(file)
+            } catch { }
+          })
+        }
+      } else {
+        console.log(`Didn't handle interaction type ${interaction.type}`);
+      }
+    }
+
+  }
+}
