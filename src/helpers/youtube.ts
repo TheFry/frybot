0dbb4a7fea59b87bd842f4e1c3d750232d914765
Luke Matusiak <lukematusiak@pm.me>
2023-11-28 20:30:12 -0800

    Rework youtube helper
    Changes
     - Make the request function more generic. Just return the response,
       don't try to format it to a YTSearchResult[]
     - Search/List now support playlists. YTSearchResult now includes type
       which is either video or playlist
     - Add function to convert playlist id to a list of videos
     - Download function writes the stream directly to a file rather than
       storing the entire file in a buffer before writing. Should decrease
       memory usage (especially on large videos)
     - Clip, play, and play-many have been updated to use the new
       search/list function parameters

--- src/helpers/youtube.ts
+++ src/helpers/youtube.ts
@@ -1,18 +1,24 @@
 import axios from 'axios';
 import ytdl from '@distube/ytdl-core';
-import fs, { ReadStream } from 'fs';
+import { ReadStream, appendFileSync, createReadStream } from 'fs';
 import { LogType, logConsole } from './logger';
+import { FileHandle, open } from 'fs/promises';
+import { Readable } from 'stream';
 
 const SEARCH_ENDPOINT = 'https://www.googleapis.com/youtube/v3/search';
-const LIST_ENDPOINT = 'https://www.googleapis.com/youtube/v3/videos';
+const VIDEO_ENDPOINT = 'https://www.googleapis.com/youtube/v3/videos';
+const PLAYLIST_ENDPOINT = 'https://www.googleapis.com/youtube/v3/playlistItems';
+const MAX_RESULTS = 250;
+const MAX_PER_PAGE = 50;    // Upper bound on results returned in a single youtube api requ
 
 export interface YTSearchResult {
-  name: string,
-  id: string
+  name: string;
+  id: string;
+  type: 'video' | 'playlist';
 }
 
 
-async function request(url: string, params: any): Promise<YTSearchResult[]> {
+async function request(url: string, params: any): Promise<any> {
   let res;
   try {
     res = await axios.get(url, {
@@ -21,69 +27,122 @@ async function request(url: string, params: any): Promise<YTSearchResult[]> {
     });
   } catch(err: any) {
     if(err.response) {
-      logConsole({ msg: err.response.data, type: LogType.Error });
+      logConsole({ msg: `${JSON.stringify(err.response.data)}`, type: LogType.Error });
       logConsole({ msg: err.response.status, type: LogType.Error });
-      logConsole({ msg: err.response.headers, type: LogType.Error });
     } else {
       logConsole({ msg: err, type: LogType.Error });
     }
-    return[];
+    return { data: { items: [] } };
   }
-  const vidData: Array<YTSearchResult> = [];
-  for(const item of res.data.items) {
-    vidData.push({
-      name: item.snippet.title,
-      id: item.id.videoId ? item.id.videoId : item.id,
-    })
-  }
-  return vidData;
+  return res.data;
 }
 
 
-export async function search(query: string, count: number, key: string): Promise<Array<YTSearchResult>> {
-  return await(request(SEARCH_ENDPOINT, {
-    q: query,
-    key: key,
-    type: 'video',
-    part: 'snippet',
-    maxResults: count
-  }));
-}
+export async function search(query: string, count: number, type: 'video' | 'playlist', key: string): Promise<YTSearchResult[]> {
+  const results: YTSearchResult[] = [];
+  count = count > MAX_RESULTS ? MAX_RESULTS : count;
+  let maxResults = count <= MAX_PER_PAGE ? count : MAX_PER_PAGE;
+  let numPages = Math.ceil(count / MAX_PER_PAGE);
 
-
-export async function list(ids: string[], key: string): Promise<Array<YTSearchResult>> {
-  let results: YTSearchResult[] = [];
-  while(ids.length > 0) {
-    let batch = await(request(LIST_ENDPOINT, {
-      id: ids.splice(0, 50).join(','),
-      key,
-      type: 'video',
-      part: 'snippet,id',
+  for(let i = 0; i < numPages; i++) {
+    let data = await(request(SEARCH_ENDPOINT, {
+      q: query,
+      key: key,
+      type: type,
+      part: 'snippet',
+      maxResults: maxResults,
     }));
-    results = results.concat(batch);
+
+    for(let item of data.items) {
+      results.push({
+        name: item.snippet.title,
+        id: item.id.kind === 'youtube#video' ? item.id.videoId : item.id.playlistId,
+        type: item.id.kind === 'youtube#video' ? 'video' : 'playlist'
+      })
+      
+      if(results.length >= count) {
+        return results;
+      }
+    }
   }
   return results;
 }
 
 
-export async function download(songId: string, path?: string): Promise<fs.ReadStream> {
+export async function list(ids: string[], type: 'video' | 'playlist', key: string): Promise<YTSearchResult[]> {
+  let results: YTSearchResult[] = [];
+  const maxIds = 50;
+  const endpoint = type === 'video' ? VIDEO_ENDPOINT : PLAYLIST_ENDPOINT;
+  
+  while(ids.length > 0) {
+    let data = await(request(endpoint , {
+      id: ids.splice(0, maxIds).join(','),
+      key,
+      type: type,
+      part: 'snippet',
+    }));
+
+    for(let item of data.items)  {
+      results.push({
+        name: item.snippet.title,
+        id: item.id,
+        type: type
+      })
+    }
+  }
+  return results;
+}
+
+
+export async function playlistToVideos(playlistId: string, key: string): Promise<YTSearchResult[]> {
+  let results: YTSearchResult[] = [];
+  let nextPage = true;
+  let data;
+
+  while(nextPage) {
+    data = await(request(PLAYLIST_ENDPOINT, {
+      playlistId: playlistId,
+      key: key,
+      part: 'snippet',
+      pageToken: data ? data.nextPageToken : undefined 
+    }));
+
+    for(let item of data.items) { 
+      results.push({ 
+        name: item.snippet.title,
+        id: item.snippet.resourceId.videoId,
+        type: 'video' 
+      }) 
+    }
+    nextPage = data.nextPageToken ? true : false;
+  }
+  return results;
+}
+
+
+export async function download(songId: string, path?: string): Promise<Readable> {
   let download = ytdl(songId, { filter: 'audioonly' });
   if(!path) return download as ReadStream;
+  let f: FileHandle;
+
+  try {
+    f = await open(path, 'w+');
+  } catch(err) {
+    logConsole({ msg: `Error opening ${path} - ${err}`, type: LogType.Error })
+  }
+  
   return new Promise((resolve, reject) => {
-    let buff: any[] | null = [];
+    if(!f) reject(null);
     download.on('data', (chunk) => {
-      if(buff) buff.push(chunk);
-    })
+      appendFileSync(path, chunk);
+    });
     download.once('end', () => {
-      if(buff) {
-        fs.writeFileSync(path, Buffer.concat(buff));
-      }
-      buff = null;
-      resolve(fs.createReadStream(path));
+      resolve(f.createReadStream({autoClose: true}));
     });
     download.on('error', (err) => {
       logConsole({ msg: `${err}`, type: LogType.Error });
+      f.close();
       reject(null);
-    })
+    });
   })
 }
