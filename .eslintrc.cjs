c92bdc96f4482ea73db0f5c0672831c32fdb87b5
Luke Matusiak <lukematusiak@pm.me>
2024-01-12 16:48:38 -0800

    target es2021 to recognize global module

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,4 +1,8 @@
 module.exports = {
+  env: {
+    node: true,
+    es2021: true
+  },
   extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
