2b6cd7ac6211b51f06405e1dbc0635a25a879980
Luke Matusiak <lukematusiak@pm.me>
2025-04-01 14:01:23 -0700

    a very basic chat bot with AWS bedrock

---
+++ src/helpers/ChatBot.ts
@@ -0,0 +1,15 @@
+export class ChatBot {
+  constructor() {}
+  public async generateText() {
+    console.log("Not supported");
+    throw Error("Not Supported!")
+  }
+  public async textToSpeech() {
+    console.log("Not supported");
+    throw Error("Not Supported!")
+  }
+  public async textToImage() {
+    console.log("Not supported");
+    throw Error("Not Supported!")
+  }
+}
